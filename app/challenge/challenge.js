85dc7c759486c4b7520bb31c5c4733a67a642d0c
zzmp <zmp@umich.edu>
2014-09-07 02:07:48 -0700

    Paypal button

--- app/challenge/challenge.js
+++ app/challenge/challenge.js
@@ -5,12 +5,32 @@ angular.module('app.challenge', [
   'ui.bootstrap'
 ])
 
-.controller('ChallengeModalCtrl', function($scope, $modalInstance) {
-  $scope.ok = function() {
-    $modalInstance.close();
+.controller('ChallengeModalCtrl', function($rootScope, $scope, $modalInstance, $http, $timeout, twitter_handle) {
+  $scope.ok = function(amount) {
+    var node = angular.element(document.createElement('div'));
+    var button = angular.element([
+      '<script src="//www.paypalobjects.com/js/external/paypal-button.min.js?merchant=challengewithfriends@gdi2290.com"',
+        'data-button="donate"',
+        'data-name="Breast Cancer Awareness Month"',
+        'data-amount="' + amount + '"',
+        'data-currency="USD"',
+        'data-callback="http://challengewithfriends.com/paid/' + twitter_handle + '"',
+        'data-env="sandbox"',
+      '></script>'].join(' '));
+    node.append(button);
+    angular.element(document.querySelector('div')).append(node);
+    // Load the element (using the ng-if in index.html)
+    $rootScope.donate = true;
+
+    $timeout(function() {
+      node.find('button').click();
+    });
   };
 
   $scope.cancel = function() {
     $modalInstance.dismiss('cancel');
   };
 });
+
+
+
