5ebe796abd637db39499cca881458486d2608f7c
gdi2290 <github@gdi2290.com>
2014-09-06 17:28:04 -0700

    feat(navbar,footer): include navbar and footer

--- app/app.js
+++ app/app.js
@@ -15,21 +15,37 @@ angular.module('app', [
       url: '',
       abstract: true,
       views: {
+        'navbar': {
+          templateUrl: 'layout/navbar.html',
+          controller: 'NavbarCtrl'
+        },
         'main': {
           templateUrl: 'app.html',
           controller: 'AppCtrl'
+        },
+        'footer': {
+          templateUrl: 'layout/footer.html',
+          controller: 'FooterCtrl'
         }
       }
     });
 
   $urlRouterProvider
     // .when('', '/home')
-    .otherwise('/home');
+    .otherwise('/dashboard');
 
   // $locationProvider.html5Mode(true);
 })
 
 
+.controller('NavbarCtrl', function() {
+
+})
+
 .controller('AppCtrl', function() {
+
+})
+
+.controller('FooterCtrl', function() {
 
 });
