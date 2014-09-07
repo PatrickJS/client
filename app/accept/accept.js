2153b18c4960890f7f3264d556ded67b7b5f3e59
zzmp <zmp@umich.edu>
2014-09-07 09:21:57 -0700

    Cleaned up final modal

--- app/accept/accept.js
+++ app/accept/accept.js
@@ -8,24 +8,8 @@ angular.module('app.accept', [
 .controller('AcceptModalCtrl', function($scope, $modalInstance, $http, youtubeEmbedUtils, auth, twitter) {
   $scope.twitter_handle = twitter;
 
-  $scope.ok = function(youtube) {
+  $scope.ok = function(youtube, handle1, handle2, handle3) {
     var youtube_id = youtubeEmbedUtils.getIdFromURL(youtube);
-    $modalInstance.close({twitter: twitter, youtube: youtube_id});
-  };
-
-  $scope.cancel = function() {
-    $modalInstance.dismiss('cancel');
-  };
-
-  $scope.respond = function(response, youtube) {
-    console.log(auth);
-    $http.post('/tweet', {
-      status: [response, youtube].join(' '),
-      token: auth.profile.identities[0].access_token,
-      tokenSecret: auth.profile.identities[0].access_token_secret });
-  };
-
-  $scope.nominate = function(youtube, handle1, handle2, handle3) {
     var handles = [handle1, handle2, handle3];
     handles = handles.map(function(handle) {
       if (handle) {
@@ -39,5 +23,10 @@ angular.module('app.accept', [
       status: [handles, 'You have been challenged!', youtube].join(' '),
       token: auth.profile.identities[0].access_token,
       tokenSecret: auth.profile.identities[0].access_token_secret });
+    $modalInstance.close({twitter: twitter, youtube: youtube_id});
+  };
+
+  $scope.cancel = function() {
+    $modalInstance.dismiss('cancel');
   };
 });
