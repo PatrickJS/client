c5672765ec7fd93e5ccf034b088f6dca59826a45
zzmp <zmp@umich.edu>
2014-09-07 04:15:05 -0700

    Debug AngularFire ngRepeats

--- app/components/resources/donated.js
+++ app/components/resources/donated.js
@@ -3,7 +3,7 @@
 angular.module('Donated', [])
 
 .factory('Donated', function(Firebase, $firebase) {
-  var ref = new Firebase('https://ChallengeWithFriends.firebaseio.com/dontaed');
+  var ref = new Firebase('https://ChallengeWithFriends.firebaseio.com/donated');
   // create an AngularFire reference to the data
   return $firebase(ref);
 });
