7adde24e9c4c29467a137c33163d6bde62de1b1a
zzmp <zmp@umich.edu>
2014-09-07 03:02:35 -0700

    Add amount to paypal callback

--- app/donate/donate.js
+++ app/donate/donate.js
@@ -9,8 +9,8 @@ angular.module('app.donate', [
   $scope.ok = function(amount) {
     var form = $('#paypalform');
     form.find('[name="amount"]').val(amount);
-    form.find('[name="notify_url"]').val(
-      'http://challengewithfriends.com/paid/' + twitter);
+    form.find('[name="notify_url"]').val([
+      'http://challengewithfriends.com/paid', twitter, amount].join('/'));
     form.find('button').click();
   };
 
