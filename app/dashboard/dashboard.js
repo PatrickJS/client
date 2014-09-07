55bc03380fdb27d9074a449be6c8b213683202b3
gdi2290 <github@gdi2290.com>
2014-09-06 22:25:22 -0700

    refactor(dashbord): include helper data

--- app/dashboard/dashboard.js
+++ app/dashboard/dashboard.js
@@ -15,8 +15,75 @@ angular.module('app.dashboard', [
 
 })
 
-.controller('DashboardCtrl', function($scope) {
+.controller('DashboardCtrl', function($scope, $modal) {
+
+  $scope.awareness = {
+    name: 'Yolo Swag',
+    org: 'Swag Org'
+  };
+
+  $scope.challenge = function() {
+   var counterOffersModal = $modal.open({
+      // there is no templateProvider but you can return a promise
+      templateUrl: 'challenge/challenge.html',
+      controller: 'ChallengeModalCtrl',
+      size: 'lg',
+      resolve: {
+      }
+
+    });
+    counterOffersModal.result.then(function() {
+
+    })['catch'](function() {
+
+    });
+  };
 
   $scope.awarenessVideo = 'sMKoNBRZM1M';
+
+  // should be csss nth-child
+  $scope.isLine = function(index) {
+    return index % 4 === 3;
+  };
+
+  $scope.completed = {
+    donated: [
+      {
+        name: 'Patrick',
+        ammount: '5677'
+      },
+      {
+        name: 'Jack Ryder',
+        ammount: '456'
+      },
+      {
+        name: 'Yolo Swag',
+        ammount: '879'
+      },
+      {
+        name: 'Yolo Swag',
+        ammount: '879'
+      },
+      {
+        name: 'Yolo Swag',
+        ammount: '879'
+      },
+      {
+        name: 'Yolo Swag',
+        ammount: '879'
+      }
+    ],
+    youtubes: [
+      { image: 'bQVoAWSP7k4'},
+      { image: 'bQVoAWSP7k4'},
+      { image: 'bQVoAWSP7k4'},
+      { image: 'bQVoAWSP7k4'},
+
+      { image: 'bQVoAWSP7k4'},
+      { image: 'bQVoAWSP7k4'},
+      { image: 'bQVoAWSP7k4'},
+      { image: 'bQVoAWSP7k4'}
+    ]
+  };
 
 });
